62a6461e2491148d29fbc2c89038d0b51910ca68
吴杰 <1220253798@qq.com>
2024-08-13 18:04:32 +0800

    feat: api

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,36 +1,25 @@
 import type { AxiosRequestConfig } from 'axios'
 import axios from 'axios'
-import router from '@/router'
 
-export const baseURL = import.meta.env.VITE_BASE_URL
+export const baseURL = ''
 export const uploadURL = `${baseURL}/file/upload`
 
 const request = axios.create({
-  baseURL, // url = base url + request url
+  baseURL,
   timeout: import.meta.env.DEV ? 1000 : 5000, // request timeout
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
-let loadingMap: Record<string, ReturnType<typeof ElLoading.service>> = {}
-
 request.interceptors.request.use(
   (config) => {
     if (!config.url?.includes('upload') && !(config.data instanceof FormData)) {
       config.data = {
-        // @ts-expect-error reqHdr
-        reqHdr: config.reqHdr ?? {},
         reqBody: config.data,
       }
     }
 
-    // @ts-expect-error loading存在
-    if (config.loading !== false) {
-      loadingMap = {
-        [config.url!]: ElLoading.service({ fullscreen: true }),
-      }
-    }
     return config
   },
   (error) => {
@@ -44,8 +33,6 @@ request.interceptors.response.use(
   (response) => {
     const res = response.data
 
-    loadingMap[response.config.url!]?.close()
-
     if (res instanceof Blob) {
       return response.data
     }
@@ -53,18 +40,11 @@ request.interceptors.response.use(
       if (res.resHdr.resCode === '0000') {
         // @ts-expect-error message存在
         if (response.config.message !== false) {
-          ElMessage.success(res.resHdr.resMsg)
+          // ElMessage.success(res.resHdr.resMsg)
         }
       }
       else {
-        ElMessage.error(res.resHdr.resMsg || '')
-
         console.error(res)
-        if (res.resHdr.resCode === '9990_10') {
-          console.error(res)
-          console.log(router)
-          router.replace('/login')
-        }
       }
       return {
         resCode: res.resHdr.resCode,
@@ -76,11 +56,6 @@ request.interceptors.response.use(
   (error) => {
     console.error({ error })
 
-    for (const iterator in loadingMap) {
-      loadingMap[iterator].close()
-    }
-
-    ElMessage.error(`${error.message} 服务器错误，请重试`)
     return Promise.resolve({
       resCode: error.code,
       resMsg: error.message,
@@ -96,9 +71,6 @@ export interface Result<T> {
 }
 
 export interface RequestConfig {
-  message?: boolean
-  loading?: boolean
-  reqHdr?: Record<string, any>
 }
 
 export function get<T = never>(url: string, config?: AxiosRequestConfig & RequestConfig): Promise<Result<T>> {
